f00f39b3eabfc5bfebf41e34ef97f95121ad30f6
JulienGrade <gradejulien@gmail.com>
2024-03-23 17:08:06 +0100

    Add meteo request and location

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,7 +1,46 @@
 import { Text, View } from "react-native";
 import { s } from "./Home.style";
+import {
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+} from "expo-location";
+import {useEffect, useState} from "react";
+import { MeteoAPI } from "../../api/meteo";
 
 export function Home({}) {
+    const [coords, setCoords] = useState();
+    const [weather, setWeather] = useState();
+
+    useEffect(() => {
+        getUserCoords();
+    }, []);
+
+    useEffect(() => {
+        if (coords) {
+            fetchWeather(coords);
+        }
+    }, [coords]);
+
+    async function fetchWeather(coordinates) {
+        const weatherResponse = await MeteoAPI.fetchWeatherFromCoords(
+            coordinates
+        );
+        setWeather(weatherResponse);
+    }
+
+    async function getUserCoords() {
+        let { status } = await requestForegroundPermissionsAsync();
+        if (status === "granted") {
+            const location = await getCurrentPositionAsync();
+            setCoords({
+                lat: location.coords.latitude,
+                lng: location.coords.longitude,
+            });
+        } else {
+            setCoords({ lat: "50.58", lng: "3.06" });
+        }
+    }
+    console.log(weather);
     return (
         <>
             <View style={s.meteo_basic}>
